34143d8120006bb75de7682248b5b1707172930c
Pablo Galvan <103456849+GalvanPablo@users.noreply.github.com>
2024-10-06 19:24:01 -0300

    Fix CORS OrdenCompraController

--- src/main/java/com/proveedor/controllers/OrdenCompraController.java
+++ src/main/java/com/proveedor/controllers/OrdenCompraController.java
@@ -2,15 +2,18 @@ package com.proveedor.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.proveedor.services.OrdenCompraService;
 
 @RestController
 @RequestMapping("/ordenes")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class OrdenCompraController {
 
     @Autowired
